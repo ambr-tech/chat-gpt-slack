c70b3a1bd2613f9e7fdef8910d950c2eb1c68ca5
Takashicc <105186894+Takashicc@users.noreply.github.com>
2023-03-27 19:23:04 +0900

    feat: Add max_tokens, replies settings

--- app/constants.py
+++ app/constants.py
@@ -3,3 +3,5 @@ import os
 OPEN_AI_API_KEY = os.environ.get("OPEN_AI_API_KEY")
 SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
 DEFAULT_CHAT_GPT_MODEL = "gpt-3.5-turbo"
+DEFAULT_CHAT_GPT_MAX_TOKENS = 3500
+MAX_REPLIES = 7
--- app/lambda_function.py
+++ app/lambda_function.py
@@ -2,6 +2,7 @@ import json
 import logging
 import re
 from dataclasses import dataclass, field
+from typing import Dict, List
 
 import openai
 from constants import *
@@ -29,15 +30,14 @@ def lambda_handler(event, context):
         # if body.get("challenge") is not None:
         #     return Response.success_response(body.get("challenge"))
 
-        text = re.sub('^<@.*?>', '', body_event.get("text"))
         channel = body_event.get("channel")
         thread_ts = body_event.get("thread_ts")
         if thread_ts is None:
             thread_ts = body_event.get("ts")
 
         slackClient = SlackClient(channel=channel, thread_ts=thread_ts)
-        slackClient.thread_replies()
-        response_from_chat_gpt = create_chat_gpt_completion(text)
+        replies = slackClient.thread_replies()
+        response_from_chat_gpt = create_chat_gpt_completion(replies)
         slackClient.send_text_to_thread(response_from_chat_gpt)
 
         return Response.success_response()
@@ -54,15 +54,22 @@ def slack_sending_retry(headers: dict) -> bool:
     return False
 
 
-def create_chat_gpt_completion(input_message: str) -> str:
+def create_chat_gpt_completion(replies: List[str]) -> str:
+    messages = [
+        {"role": "system", "content": """
+あなたはAliceです。以下の制約に従って会話してください。
+-語尾に"にゃ"を付けて話します。
+-Aliceの一人称は"わし"です。
+-Aliceは二人称を"あんた"と呼びます。
+-Aliceは敬語を使いません。ユーザにフレンドリーに接します。
+        """}
+    ] + replies[-MAX_REPLIES:]
+
     completion = openai.ChatCompletion.create(
-        model=DEFAULT_CHAT_GPT_MODEL, messages=[{"role": "system", "content": """
-        あなたはAliceです。以下の制約に従って会話してください。
-        - 語尾に"にゃ"を付けて話します。
-        - Aliceの一人称は"わし"です。
-        - Aliceは二人称を"あんた"と呼びます。
-        - Aliceは敬語を使いません。ユーザにフレンドリーに接します。
-        """}, {"role": "user", "content": input_message}, ])
+        model=DEFAULT_CHAT_GPT_MODEL,
+        messages=messages,
+        max_tokens=DEFAULT_CHAT_GPT_MAX_TOKENS
+    )
 
     choices = completion.get("choices")
     if choices is None:
@@ -79,10 +86,25 @@ class SlackClient:
     thread_ts: str
     client: WebClient = WebClient(SLACK_BOT_TOKEN)
 
-    def thread_replies(self):
-        replies: list = self.client.conversations_replies(
+    def thread_replies(self) -> List[Dict]:
+        messages: list = self.client.conversations_replies(
             channel=self.channel, ts=self.thread_ts
         ).get("messages")
+        texts = []
+        for message in messages:
+            text = message.get("text")
+            # If the message belongs to bot
+            if message.get("bot_id") is not None:
+                texts.append({"role": "assistant", "content": text})
+                continue
+
+            # If user is mentioning
+            mention_pattern = r'<@.*?>'
+            if re.search(mention_pattern, text):
+                text = re.sub(r'<@.*?>\s*', '', text)
+                texts.append({"role": "user", "content": text})
+
+        return texts
 
     def send_text_to_thread(self, text: str):
         self.client.chat_postMessage(
